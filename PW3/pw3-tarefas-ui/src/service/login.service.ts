d77e8099b2c1a8026ca761ee323cd782820a4e33
Raphael567 <raphaelcanuto464@gmail.com>
2025-11-11 16:33:05 -0300

    Feat: nomes

--- PW3/pw3-tarefas-ui/src/service/login.service.ts
+++ PW3/pw3-tarefas-ui/src/service/login.service.ts
@@ -1,3 +1,6 @@
+//Raphael Pereira Canuto
+//Hellen Novi Salvador
+
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
